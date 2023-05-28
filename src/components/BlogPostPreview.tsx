8bafc05bbd5cda5a3d9c876ba23b39ce8bded29b
James Little <littleguy23@gmail.com>
2023-05-28 16:23:09 -0400

    Make whole blog post preview a link

--- src/components/BlogPostPreview.tsx
+++ src/components/BlogPostPreview.tsx
@@ -7,22 +7,22 @@ export const BlogPostPreview = ({ post }: { post: BlogPost }) => {
   const { title, outdated, blurb } = post.metadata;
 
   return (
-    <Subgrid
-      weight="right"
-      className={classNames("blog-post-preview", {
-        "blog-post-preview-outdated": outdated,
-      })}
-    >
-      <Left>
-        <p className="blog-post-preview-date">{post.formattedDate()}</p>
-      </Left>
-      <Right className="flex-container">
-        <p className="blog-post-preview-title">
-          <Link href={post.href()}>{title}</Link>
-        </p>
-        {outdated && <span className="tag">Outdated</span>}
-        {!outdated && <p>{blurb}</p>}
-      </Right>
-    </Subgrid>
+    <Link href={post.href()} className="blog-post-preview-link">
+      <Subgrid
+        weight="right"
+        className={classNames("blog-post-preview", {
+          "blog-post-preview-outdated": outdated,
+        })}
+      >
+        <Left>
+          <p className="blog-post-preview-date">{post.formattedDate()}</p>
+        </Left>
+        <Right className="flex-container">
+          <p className="blog-post-preview-title">{title}</p>
+          {outdated && <span className="tag">Outdated</span>}
+          {!outdated && <p>{blurb}</p>}
+        </Right>
+      </Subgrid>
+    </Link>
   );
 };
